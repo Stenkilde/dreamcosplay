3304b576632efcd82987c8dd586e258a355a4799
Oliver Stenkilde <oliver@stenkilde.com>
2014-09-12 19:52:56 +0200

    Adding more routing

---
+++ src/app/partials/team/team.js
@@ -0,0 +1,23 @@
+/*================================================================
+=>                  Controller = Team
+==================================================================*/
+/*global app*/
+
+(function() {
+    'use strict';
+
+    angular
+        .module('dreamcosplay')
+        .controller('TeamCtrl', TeamCtrl);
+
+    TeamCtrl.$inject = ['$scope', '$state'];
+
+    function TeamCtrl($scope, $state) {
+
+    };
+
+})();
+
+/*-----  End of Controller = Team  ------*/
+
+
